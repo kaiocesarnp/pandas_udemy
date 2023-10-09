f38ea247f374dc02910816db2a6bd23ca90f7344
kaiocesarnp <kaiocesarnp@gmail.com>
2023-10-09 12:17:00 -0300

    pdUdm

--- plotGraf.py
+++ plotGraf.py
@@ -10,24 +10,30 @@ import pandas as pd
 plt.rcParams['figure.figsize'] = (12, 7) # 20 é a parte X, 7 é Y
 
 d = pd.read_csv('c:/users/inss/desktop/k/titanic_train.csv')
-print(d.head(4))
-print(d.shape)
+#print(d.head(4))
+#print(d.shape)
 
 #plt.plot(d.Age, '*-r') #gráfico com '*' nas pontas e r = red/vermelho
-plt.title('Infos dos passageiros do Titanic')
-plt.xlabel('Passageiro', size = 20)
-plt.ylabel('Idade', size = 25)
+#plt.title('Infos dos passageiros do Titanic')
+#plt.xlabel('Passageiro', size = 20)
+#plt.ylabel('Idade', size = 25)
 #plt.show()
 
 #Utilizando a biblio pandas diretamente:
 #d.Age.plot()
-plt.xlabel('Passageiro', size = 20)
-plt.ylabel('Idade', size = 25)
+#plt.xlabel('Passageiro', size = 20)
+#plt.ylabel('Idade', size = 25)
 #plt.show()
 
 #Scatter plot. Mostra como os dados estão espalhados no gráfico
-print(d.head(2))
-plt.scatter(d.PassengerId, d.Age, color = 'g', marker = '*') #eixo X é 'passe..' e o Y é 'Age'
+#print(d.head(2))
+#plt.scatter(d.PassengerId, d.Age, color = 'g', marker = '*') #eixo X é 'passe..' e o Y é 'Age'
+#plt.show()
+
+#Histogramas: distribuição de frequência dos dados
+print(d.head(3))
+print(d.Age.describe())
+d.Age.hist()
+plt.xlabel('Idade')
+plt.ylabel('Freq observada')
 plt.show()
-
-
