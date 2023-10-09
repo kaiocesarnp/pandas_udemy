f54380cb66dbae56e868d228b8fa03b3957246f8
kaiocesarnp <kaiocesarnp@gmail.com>
2023-10-09 12:11:53 -0300

    pdUdm

---
+++ plotGraf.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt #para exibir gráficos
+import pandas as pd
+
+#print(plt.style.available)#estilos de gráficos disponíveis. Também podem ser encontrados no site matplotlib.org
+#print(plt.style.use()) #para utilizar o estilo desejado
+
+
+#Tamanho do retangulo/caixa da figura:
+plt.rcParams['figure.figsize'] = (12, 7) # 20 é a parte X, 7 é Y
+
+d = pd.read_csv('c:/users/inss/desktop/k/titanic_train.csv')
+print(d.head(4))
+print(d.shape)
+
+#plt.plot(d.Age, '*-r') #gráfico com '*' nas pontas e r = red/vermelho
+plt.title('Infos dos passageiros do Titanic')
+plt.xlabel('Passageiro', size = 20)
+plt.ylabel('Idade', size = 25)
+#plt.show()
+
+#Utilizando a biblio pandas diretamente:
+#d.Age.plot()
+plt.xlabel('Passageiro', size = 20)
+plt.ylabel('Idade', size = 25)
+#plt.show()
+
+#Scatter plot. Mostra como os dados estão espalhados no gráfico
+print(d.head(2))
+plt.scatter(d.PassengerId, d.Age, color = 'g', marker = '*') #eixo X é 'passe..' e o Y é 'Age'
+plt.show()
+
+
